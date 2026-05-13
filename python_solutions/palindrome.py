84aa056a5aee2dcf94bdb900c0558518f83746c5
altoids <noelreeds01@gmail.com>
2026-05-13 18:47:17 +0300

    return true if an integer is a palindrome, otherwise false

--- python_solutions/palindrome.py
+++ python_solutions/palindrome.py
@@ -1,6 +1,25 @@
-class Solution(object):
-    """Soln Class for Palindrome task"""
-
-    def isPalindrome(self, x: int) -> bool:
-        """Returns True for a palindrome, otherwise False"""
-        pass
+class Solution:
+	def isPalindrome(self, x: int) -> bool:
+		if x <= 0:
+			return False
+		rx = x
+		d_x = 0
+		value_x = 0
+		place_value = 1
+		while(x):
+			d_x = x % 10
+			value_x += d_x * place_value
+			if x >= 10:
+				place_value *= 10
+			x //= 10
+		value_rx = 0
+		d_rx = 0
+		while(place_value):
+			d_rx = rx % 10
+			value_rx += d_rx * place_value
+			rx //= 10
+			place_value //= 10
+		if value_x != value_rx:
+			return False
+		else:
+			return True
